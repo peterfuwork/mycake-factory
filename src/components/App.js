4dde0fa9b805235b5931ffaf2175b63ee54f7fc1
Chiaen Fu <peterfuwork@gmail.com>
2018-11-02 14:09:45 -0600

    removed store files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Cards from './Cards';
-import { observable } from "mobx";
-import { observer } from "mobx-react";
 
 class App extends Component {
   constructor() {
